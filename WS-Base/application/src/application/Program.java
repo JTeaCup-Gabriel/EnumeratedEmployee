76d6bb22924b3a538f886566c370c37d65f975a2
Gabriel Ferreira <GF_JTeaCup_BR@outlook.com>
2019-12-09 12:18:18 -0300

    Início de programação da classe principal

--- WS-Base/application/src/application/Program.java
+++ WS-Base/application/src/application/Program.java
@@ -1,9 +1,54 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Department;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		
+		// -------------------------------------------------------------------- >
+		System.out.print("Enter department's name: ");
+		String departmentName = sc.nextLine();
+
+		System.out.println("Enter worker data:");
+
+		System.out.print("Name: ");
+		String workerName = sc.nextLine();
+
+		System.out.print("Level: ");
+		String workerLevel = sc.nextLine();
+
+		System.out.print("Base salary: ");
+		double baseSalary = sc.nextDouble();
+		// instanciamento do funcionário que vai receber os atributos
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+		
+
+		// -------------------------------------------------------------------- >
+		System.out.print("How many contracts to this worker? ");
+		int N = sc.nextInt();
+		// -------------------------------------------------------------------- >
+		for (int i = 1; i <= N; i++) {
+			System.out.println("Enter contract #" + i + " data:");
+			System.out.println("Date (DD/MM/YYYY): ");
+			
+			
+		}
+		// -------------------------------------------------------------------- >
+		// -------------------------------------------------------------------- >
+		sc.close();
 
 	}
 
